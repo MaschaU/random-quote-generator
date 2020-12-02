ee2518c71944f69e4abaad4d07a024950f9a1064
Masa Utkovic <masa.utkovic@gmail.com>
2020-12-02 11:17:42 +0100

    add random color function

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.getElementById("new-quote").addEventListener("click", function (e) {
-    console.log("BANG!!");
+// document.getElementById("new-quote").addEventListener("click", function (e) {
+//     console.log("BANG!!");
+// });
+
+$(document).ready(function() { 
+    console.log("Something's working");
+    var newColor = function getNewColor() {
+        var letters = '012345';
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 6)];
+        }
+        return color;
+    }
+
+    $("body").css("color",newColor); 
 });
+                            
+    
+
