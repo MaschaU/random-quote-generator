9eb29875484e3e7e23239a755ae134acd2b2e030
Masa Utkovic <masa.utkovic@gmail.com>
2020-12-03 19:25:24 +0100

    changing background colors on load and click

--- script.js
+++ script.js
@@ -2,19 +2,46 @@
 //     console.log("BANG!!");
 // });
 
-$(document).ready(function() { 
-    console.log("Something's working");
-    var newColor = function getNewColor() {
-        var letters = '012345';
-        var color = '#';
-        for (var i = 0; i < 6; i++ ) {
-            color += letters[Math.floor(Math.random() * 6)];
-        }
-        return color;
-    }
 
-    $("body").css("color",newColor); 
+function getNewColor() {
+    // console.log("Something's working here as well");
+    var letters = '012345';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 6)];
+    }
+    return color;
+}
+
+function changeBackgroundColor() {
+    var newColor = getNewColor();
+    $("body").css("background-color", newColor);
+}
+
+$(document).ready(function() { 
+    // console.log("Something's working");
+    // once the document is ready, select the button and attach the click handler
+    // pass the changeBackgrounColor function to the click handler
+    $("#new-quote").click(changeBackgroundColor);
+    // call the changeBackground function when the document is ready
+    changeBackgroundColor(); 
 });
+
+
+
+
+
+
+
+// $("#newQuote").click(function() {      
+//     $.getJSON("text/quotes.json", function(responseText) { 
+      
+//       $("#quoteText").html('"' + responseText.quoteText + '"');
+//       $("#author").html("- " + responseText.quoteAuthor);
+      
+//       newQuote = responseText.quoteText;
+//   });                    
+// });
                             
     
 
